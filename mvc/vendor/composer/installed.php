9fee7f382dd8708164c8795ffaf1e31a54d6183d
André Cardoso <andre@spzn.pt>
2020-10-30 17:28:15 +0000

    composer

---
+++ mvc/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '32c5209e6a9ba687165b1cec7787a4758517988f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '32c5209e6a9ba687165b1cec7787a4758517988f',
+    ),
+  ),
+);
