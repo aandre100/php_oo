ce32bed2f33cecd2ad42ad7d66d3461411060e4d
André Cardoso <andre@spzn.pt>
2020-10-30 11:59:03 +0000

    more coments

--- protected/Funcionario.php
+++ protected/Funcionario.php
@@ -5,20 +5,28 @@
  */
 class Funcionario
 {
-	/** @var string $nome recebe o nome do colaborador */
+	/** $nome recebe o nome do colaborador */
 	public string $nome;
-	/** @var float $salario recebe o salario do colaborador */
+	/**  $salario recebe o salario do colaborador */
 	public float $salario;
-	/** @var string $salarioConvertido recebe o salario convertido do colaborador */
+	/** $salarioConvertido recebe o salario convertido do colaborador */
 	private string $salarioConvertido;
-	/** @var string $bonus recebe o bonus do colaborador */
+	/** $bonus recebe o bonus do colaborador */
 	protected float $bonus = 2500;
 	/** 
-	 * Ver salário mostra o salário do funcionário já comnvertido com number format
+	 *  Retorna a frase e salário do funcionário já comnvertido com number format
+	 * 
+	 * @return string retorna a frase com o salário
 	 */
 	public function verSalario(): string {
-		return "O Funcionário {$this->nome} aufere o salário de {$this->converterSalario($this->salario)}€";
+		return "O Funcionário {$this->nome} aufere o salário de {$this->converterSalario($this->salario)} <br>";
 	}
+	/**
+	 * Recebe o salário e retorna convertido
+	 * Método privado, somente pode ser chamado na classe
+	 * @param float $valor deve ser enviado parametro numerico
+	 * @return string Retorna o valor convertido
+	 */
 	private function converterSalario($valor): string{
 		$this->salarioConvertido = number_format($valor, '2', ',', '.');
 		return $this->salarioConvertido;
